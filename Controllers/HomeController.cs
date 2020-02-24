79f6b90059710891ba847ff772f93a581d6adafa
abraham <abehamdan34@gmail.com>
2020-02-24 09:56:16 -0500

    turn in

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MVC_Lab____.Models;
+
+namespace MVC_Lab____.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult MileageForm()
+        {
+            return View();
+        }
+
+        public IActionResult MileageResult(double mpg, double gallons)
+        {
+            var model = new MileageResultModel()
+            {
+                MPG = mpg,
+                Gallons = gallons
+            };
+            return View(model);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/MileageResultModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Lab____.Models
+{
+    public class MileageResultModel
+    {
+        public double MPG { get; set; }
+        public double Gallons { get; set; }
+        public double Total => MPG * Gallons;
+    }
+}
